549c41439362e26b79e5816eb524ff533bb56826
Daniel Caldito Serrano <dcs1990x@gmail.com>
2025-11-05 11:53:52 +0100

    feat: added basic health check endpoint

---
+++ src/main/java/cat/itacademy/s04/t01/Level1/userapi/UserapiApplication.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t01.Level1.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserapiApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(UserapiApplication.class, args);
+	}
+}
